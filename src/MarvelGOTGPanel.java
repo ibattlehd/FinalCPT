d8fcad60f88a2739c2b113a30130e677da560ab4
Nick <44984435+ibattlehd@users.noreply.github.com>
2019-01-08 10:22:07 -0500

    Created src folder

---
+++ src/MarvelGOTGPanel.java
@@ -0,0 +1,28 @@
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
+import javax.imageio.*;
+import java.io.*;
+
+public class MarvelGOTGPanel extends JPanel{
+	
+	// Properties
+	
+	
+	
+	// Methods
+	public void paintComponent(Graphics g){
+	}
+	
+	
+	
+	// Constructor
+	public MarvelGOTGPanel(){
+		super();
+	}
+	
+	
+	
+}
+	
