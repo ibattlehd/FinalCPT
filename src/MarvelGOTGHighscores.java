bffdc6599ef62b0ccc9bd740dd848796a1e4c834
Kenneth Tse <tse.kenneth79@gmail.com>
2019-01-11 10:38:39 -0500

    Testing highscores

---
+++ src/MarvelGOTGHighscores.java
@@ -0,0 +1,35 @@
+import java.io.*;
+
+public class MarvelGOTGHighscores{
+	public static void main(String[] args){
+		boolean blnFileFail = false;
+		FileReader thefile = null;
+		BufferedReader thefiledata = null;
+		try{
+	       		 thefile = new FileReader("highscores.txt");
+		}catch(FileNotFoundException e){
+			System.out.println("Unable to read from the file");
+			blnFileFail = true;
+}
+if(blnFileFail == false){		
+		thefiledata = new BufferedReader(thefile);
+
+		String strLine = "";
+		try{
+			strLine = thefiledata.readLine();
+		}catch(IOException e){
+			strLine = "testing";
+		}
+		while(strLine != null){
+			System.out.println(strLine);
+			try{
+			strLine = thefiledata.readLine();
+			}catch(IOException e){
+				strLine = "testing";
+			}
+
+		}
+	}
+
+}
+}
