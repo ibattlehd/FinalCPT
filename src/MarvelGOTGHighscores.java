23e74ba8933cafcbee6efe3b2ad132b82ee0b584
Nick <44984435+ibattlehd@users.noreply.github.com>
2019-01-12 04:20:02 -0500

    Update MarvelGOTGHighscores.java

--- src/MarvelGOTGHighscores.java
+++ src/MarvelGOTGHighscores.java
@@ -6,24 +6,24 @@ public class MarvelGOTGHighscores{
 		FileReader thefile = null;
 		BufferedReader thefiledata = null;
 		try{
-	       		 thefile = new FileReader("highscores.txt");
+			thefile = new FileReader("highscores.txt");
 		}catch(FileNotFoundException e){
 			System.out.println("Unable to read from the file");
 			blnFileFail = true;
-}
-if(blnFileFail == false){		
-		thefiledata = new BufferedReader(thefile);
-
-		String strLine = "";
-		try{
-			strLine = thefiledata.readLine();
-		}catch(IOException e){
-			strLine = "testing";
 		}
+		if(blnFileFail == false){		
+			thefiledata = new BufferedReader(thefile);
+			
+			String strLine = "";
+			try{
+				strLine = thefiledata.readLine();
+			}catch(IOException e){
+				strLine = "testing";
+			}
 		while(strLine != null){
 			System.out.println(strLine);
 			try{
-			strLine = thefiledata.readLine();
+				strLine = thefiledata.readLine();
 			}catch(IOException e){
 				strLine = "testing";
 			}
