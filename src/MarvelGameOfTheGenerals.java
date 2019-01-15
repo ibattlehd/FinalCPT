3ea61269832d88e2dd3023b3094ee62340158627
Nick <44984435+ibattlehd@users.noreply.github.com>
2019-01-15 14:41:36 -0500

    Moved GamePanel class into MarvelGameOfTheGenerals class

--- src/MarvelGameOfTheGenerals.java
+++ src/MarvelGameOfTheGenerals.java
@@ -555,6 +555,33 @@ public class MarvelGameOfTheGenerals implements ActionListener, KeyListener, Mou
 		frame.setVisible(true);
 	}
 	
+	////////////////////
+	// GAME PANEL
+	////////////////////
+	public class GamePanel extends JPanel{
+		// Properties
+		int intPage = 1;
+		BufferedImage mainmenu;
+		
+		// Methods
+		public void paintComponent(Graphics g){ // Overriding JPanel's paintComponent method
+			if(intPage == 1){
+				g.drawImage(mainmenu, 0, 0, null);
+			}
+		}
+		
+		// Constructor
+		public GamePanel(){
+			super();
+			// Images
+			try{
+				mainmenu = ImageIO.read(new File("../img/mainmenu.png"));
+			}catch(IOException e){
+				System.out.println("Unable to load image");
+			}
+		}
+	}
+	
 	
 	// Main Method
 	public static void main(String[] args){
