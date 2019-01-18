f92d3bb7d3ff00f286cd1cb83cb1843ccfb7d06d
Nick <44984435+ibattlehd@users.noreply.github.com>
2019-01-18 14:24:37 -0500

    Updated highscores Java file

--- src/MarvelGOTGHighscores.java
+++ src/MarvelGOTGHighscores.java
@@ -3,13 +3,16 @@ import java.io.*;
 /**
 * <h1>Marvel Game of the Generals Highscores</h1>
 * @author  ibattlehd (Nick)
-* @version 1.0
 */
 public class MarvelGOTGHighscores{
-	public static void main(String[] args){
+	
+	//public String[][] readMapArray(){
+	public static void main(String[]args){
 		FileReader thefile = null;
 		BufferedReader thefiledata = null;
 		boolean blnOpen = false;
+		int intNum = 100;
+		String strScores[][] = new String[1][intNum]; // holds top 100 high scores
 		
 		try{
 			thefile = new FileReader("../data/highscores.txt");
@@ -24,14 +27,19 @@ public class MarvelGOTGHighscores{
 				String strLine = thefiledata.readLine();
 				String strLines[] = strLine.split(",");
 				String[][] strLinesTxt = new String[strLines.length][];
+				intNum = strLines.length;
 				for (int intCount=0; intCount<strLines.length; intCount++) {
 					strLinesTxt[intCount] = strLines[intCount].split(",");
-					System.out.println(strLinesTxt[intCount][0]);
+				}
+				for (int intCount=0; intCount<strLines.length; intCount++) {
+					strScores[0][intCount] = strLinesTxt[intCount][0];
+					System.out.println(strScores[0][intCount]);
 				}
 			}catch(IOException e){
 				e.printStackTrace();
 			}
 		}
 		
+		//return strScores.clone();
 	}
 }
