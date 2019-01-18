5b2571ee118d1aa1a20374af56236150c2fc62e9
Nick <44984435+ibattlehd@users.noreply.github.com>
2019-01-18 14:45:15 -0500

    Update board button design

--- src/MarvelGameOfTheGenerals.java
+++ src/MarvelGameOfTheGenerals.java
@@ -401,7 +401,6 @@ public class MarvelGameOfTheGenerals implements ActionListener, KeyListener, Mou
 				button[intRow][intCol].setSize(90, 90);
 				button[intRow][intCol].setBackground(Color.BLACK); // Set background to black
 				button[intRow][intCol].setOpaque(true);
-				button[intRow][intCol].setBorderPainted(false);
 				button[intRow][intCol].addActionListener(this);
 				boardpanel.add(button[intRow][intCol]);
 			}
